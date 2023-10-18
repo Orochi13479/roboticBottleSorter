86a273cc7a01a024e48d81bf72e9b1007c98bf29
daniell-lo <daniel.lo1706@gmail.com>
2023-10-19 00:56:42 +1100

    Working pushbutton, breaks run

--- arduino_test.m
+++ arduino_test.m
@@ -1,20 +1,37 @@
 clc;
-close all
+close all;
 clear;
 
 ledPin = 'D4';
+buttonPin = 'D2';
 deltaT_blink = 0.5;
-
-port = 'COM3';
-board = 'Due';
 
 a = arduino;
 
-for k =1:10
-    a.writeDigitalPin(ledPin, 0);
-    pause(deltaT_blink/2)
+a.configurePin(buttonPin, 'Pullup');
+buttonState = 1;
+isBlinking = true;
 
-    a.writeDigitalPin(ledPin, 1);
-    pause(deltaT_blink/2);
+while isBlinking
+    buttonState = a.readDigitalPin(buttonPin);
+    if buttonState == 0
+        isBlinking = ~isBlinking; % Toggle the blinking state
+    end
+
+    if isBlinking
+        for k = 1:10
+            a.writeDigitalPin(ledPin, 0);
+            pause(deltaT_blink/2)
+
+            a.writeDigitalPin(ledPin, 1);
+            pause(deltaT_blink/2);
+
+            buttonState = a.readDigitalPin(buttonPin);
+            if buttonState == 0
+                isBlinking = ~isBlinking; % Toggle the blinking state
+                break;
+            end
+        end
+    end
 end
 
