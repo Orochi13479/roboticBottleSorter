2820615f21bcf6af8f7f1a8b4b36d50d70d6c32e
rosh218 <125846747+rosh218@users.noreply.github.com>
2023-10-18 21:02:50 +1100

    GUI addition of teach func for WidowX250

--- test.m
+++ test.m
@@ -2,14 +2,14 @@ close all;
 clear all;
 clc;
 
-r = WidowX250Gripper;
+r = WidowX250;
 rModel = r.model;
-rModel.teach(zeros(r.model.n));
+rModel.teach;
 
-qPath = jtraj([0, 0.03], [0, 0.074], 200)
-for i = 1:length(qPath)
-    rModel.animate(qPath(i, :))
-    drawnow();
-    pause(0)
-
-end
+% qPath = jtraj([0, 0.03], [0, 0.074], 200)
+% for i = 1:length(qPath)
+%     rModel.animate(qPath(i, :))
+%     drawnow();
+%     pause(0)
+% 
+% end
