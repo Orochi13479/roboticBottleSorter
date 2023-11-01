66f05415a7f7ae2498bff807c027b21816cca78e
daniell-lo <daniel.lo1706@gmail.com>
2023-11-02 00:34:58 +1100

    Adding target joints for real robot

--- test.m
+++ test.m
@@ -11,9 +11,9 @@ rModel.teach;
 % position ALSO please use waypoints or the robot will go crazy!!
 targetJointStates = [
     [pi / 8, -pi / 2, 0, -pi / 2, 0, pi / 8];
-    deg2rad([23,23,23,23,23,23]);
-    deg2rad([]);
-    deg2rad([]);
-    deg2rad([]);
+    deg2rad([117,-35.5,66,-117,-90,22.5]);
+    deg2rad([147,-56,86,-117,-90,22.5]);
+    deg2rad([198,-30,52,-117,-90,22.5]);
+    deg2rad([228,-15,35.5,-106,-90,22.5]);
     [pi / 8, -pi / 2, 0, -pi / 2, 0, pi / 8]
 ]  
